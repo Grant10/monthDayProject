bc05176869a242e375f5ef81459f3a7efad490d0
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-11-21 10:07:28 -0500

    returned the lines of the poem

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -3,7 +3,76 @@ package org.lickingheights;
 import java.util.Scanner;
 
 public class Main {
-   
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        int userYear1;
+        int userMonth;
+        int userDay2;
+        int userDay1;
+        int UserYear1;
+        int k;
+        int j;
+
+        while (true)
+
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+
+            System.out.println("Monday's child is full of face");
+            System.out.println("Tuesday's child is full of grace");
+            System.out.println("Wednasday's child is full of woe");
+            System.out.println("Thursday's child has far to go");
+            System.out.println("Friday's child is loving and giving");
+            System.out.println("Saturday's child works hard for a living");
+            System.out.println("but the child born on sabath day, is fair and good in evry way");
+            System.out.println("");
+            System.out.println("let's see what day you were born on.  What month were you born (number only please)");
+            userMonth = keyboard.nextInt();
+
+            System.out.println("What day number in " + printMonth(userMonth) + "");
+            userDay1 = keyboard.nextInt();
+            System.out.println("what year were you born");
+            userYear1 = keyboard.nextInt();
+            System.out.println("There are " + (userMonth) + " day in " + printMonth(userMonth) + "");
+            int tempIntValue = weekDay(userDay1, userMonth, userYear1);
+
+
+            System.out.println("According to the poem you are " + poemLines(tempIntValue));
+
+
+        }
+
+
+    }
+    public static int userMonth2(int userMonth) {
+
+        switch (userMonth) {
+            case 1:
+                return 13;
+            case 2:
+                return 14;
+
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                return userMonth;
+
+            default:
+                return (0);
+        }
+    }
+
     public static int weekDay(int userDay, int userMonth2, int userYear1) {
         userMonth2 = userMonth2(userMonth2);
         if (userMonth2 > 12) {
